cc1a2cebfd9f3587b87ba6588a5cd75c87d04028
alessandro <ale.signo@yahoo.com.ar>
2021-10-06 16:35:54 -0300

    Estilos finalizados pantalla de login

--- resources/views/auth/formLogin.blade.php
+++ resources/views/auth/formLogin.blade.php
@@ -1,28 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="container form-login h-100">
+<section class="container form-login">
     <div class="row justify-content-between align-items-center header-row">
-        <div class="col-md-5">
-            <h2 class="text-center">Inicia sesión con tus credenciales</h2>
-            <img src="" alt="">
+        <div class="col-md-7">
+            <img src="{{ url('img/login-img.jpg') }}" alt="foto de servidores" class="img-fluid">
         </div>
-        <div class="col-md-5">
+        <div class="col-md-4">
+            <h2 class="text-center mb-5">Inicia sesión con tus credenciales</h2>
             <form action="{{ route('auth.login') }}" method="post">
                 @csrf
                 <div class="form-group">
                     <label for="email">Email</label>
                     <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelpId" value="{{ old('email') }}">
                     @error('email')
-                        <div class="text-danger">{{ $message }}</div>
+                    <div class="text-danger">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="form-group">
                     <label for="password">Contraseña</label>
                     <input type="password" class="form-control" name="password" id="password">
                     @error('password')
-                        <div class="text-danger">{{ $message }}</div>
-                    @enderror    
+                    <div class="text-danger">{{ $message }}</div>
+                    @enderror
                 </div>
                 <button type="submit" class="btn btn-primary w-100">Ingresar</button>
             </form>
